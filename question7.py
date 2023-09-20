41a4256d2344d623955afeeab109b8a1774f3337
bonnieives <120677106+bonnieives@users.noreply.github.com>
2023-09-20 18:42:33 -0400

    Add files via upload

---
+++ question7.py
@@ -0,0 +1,16 @@
+"""
+
+    ASSIGNMENT #1
+    QUESTION 7
+    AUTHOR: Bonnie Ives de Castro Nunes
+    DATE: September 15th, 2023
+    
+"""
+
+string = "Welcome to USA. usa awesome, isn't it?"
+
+print(string)
+
+string = string.lower()
+
+print("\nThe number of occurrences of USA (case insensitive) into the informed string is:", string.lower().count("usa"))
